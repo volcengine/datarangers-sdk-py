1f3b8f67b1161622210a724f07f9686b5377729f
hezhiwei <hezhiwei.alden@bytedance.com>
2020-08-18 11:32:27 +0800

    第一个版本

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='datarangers-sdk-python',
+    version='1.0.0',
+    packages=['datarangers'],
+    url='',
+    license='DataRangers',
+    author='DataRangers',
+    author_email='DataRangers@mail.bytedance.com',
+    description='',
+    install_requires=['requests']
+)
